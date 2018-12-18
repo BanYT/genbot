493ca1a62493bc9eb39f69c461b45fd4cb3d6b54
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-18 15:38:54 +0100

    Update 8ball.js

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -47,6 +47,23 @@ var embed = new Discord.RichEmbed()
 .setColor("RANDOM")
 
 message.channel.send(embed)
+	
+	
+	
+	 if (talkedRecently.has(msg.author.id)) {
+            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
+    } else {
+
+           // the user can type the command ... your command code goes here :)
+
+        // Adds the user to the set so that they can't talk for a minute
+        talkedRecently.add(msg.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          talkedRecently.delete(msg.author.id);
+        }, 60000);
+    }
+	
 
 }
 module.exports.help = {
