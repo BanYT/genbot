6185c1a5d22de82a2ad5ba074c44f0aef26551dc
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-18 15:25:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
-Discord = require("discord.js");
-const client = new Discord.Client();
-require('./util/cmdloader.js')(client);//requires the command loader
-let token = process.env.token;
-let prefix = "!";
+const botconfig = require("./botconfig.json");
+const token = process.env.token;
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+let purple = botconfig.purple;
 let cooldown = new Set();
-let cdseconds = 900;
+let cdseconds = 5;
 
-client.on('ready', () => {
-console.log('IM READY !');
-client.user.setActivity(`${client.guilds.size} Servers With ${client.users.size} Members`, { type: "Watching" });
-        setTimeout(game2, 20000)
-    });
-    
-    function game1() {
-        client.user.setActivity(`Type .Help And See Full List Of My Commands`, { type: "Watching" });
-        setTimeout(game2, 20000)
-    }
-    
-    function game2() {
-        client.user.setActivity(`Mojang Premium Accounts From https://selly.gg/@BanGamer For Only 4€ Do Like Me!`, { type: "Buying" });
-        setTimeout(game3, 30000)
-    }
-    
-    function game3() {
-       client.user.setActivity(`Responding For ${client.commands.size} commands`, { type: "Watching" });
-        setTimeout(game1, 20000);//these times are in ms, so 30,000 = 30 seconds
-    }      //seconds/1000 = ms
-client.on('message', message => {
- if (message.author.bot) return;
- if (!message.content.startsWith(prefix)) return;
+fs.readdir("./commands/", (err, files) => {
 
-  let command = message.content.split(" ")[0];
-  cmd = command.slice(prefix.length);
-  let args = message.content.split(" ").slice(1);
-
-  
-//command handler
-let commandfile = client.commands.get(cmd);
-  let alias = client.aliases.get(cmd);
-
-  if(commandfile){
-      commandfile.run(client,message,args);
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
   }
-  if(alias){
-      alias.run(client,message,args);
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+
+bot.on("ready", async () => {
+
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
+
+});
+
+
+bot.on("message", async message => {
+
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+      prefixes: botconfig.prefix
+    };
   }
-        
-          let prefix = prefixes[message.guild.id].prefixes;
+
+
+  let prefix = prefixes[message.guild.id].prefixes;
   if(!message.content.startsWith(prefix)) return;
   if(cooldown.has(message.author.id)){
     message.delete();
@@ -67,7 +66,7 @@ let commandfile = client.commands.get(cmd);
   setTimeout(() => {
     cooldown.delete(message.author.id)
   }, cdseconds * 1000)
-        
+
 });
-     
-  client.login(process.env.token);
+
+bot.login(tokenfile.token);
