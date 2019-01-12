8155baeef7da22bb21680b1daf4293c3fd29362e
BanYT <44928120+BanYT@users.noreply.github.com>
2019-01-12 13:31:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,24 +7,15 @@ let prefix = "!";
 
 client.on('ready', () => {
 console.log('IM READY !');
-client.user.setActivity(`My Owner Sword Fighter#0001`, { type: "Watching" });
+client.user.setActivity(`Waiting My Owner`, { type: "Watching" });
         setTimeout(game2, 20000)
     });
     
     function game1() {
-        client.user.setActivity(`Type !Help And See Full List Of My Commands`, { type: "Watching" });
+        client.user.setActivity(`Waiting My Owner :(, { type: "Watching" });
         setTimeout(game2, 20000)
     }
-    
-    function game2() {
-        client.user.setActivity(`With My Owner C:`, { type: "Playing" });
-        setTimeout(game3, 30000)
-    }
-    
-    function game3() {
-       client.user.setActivity(`to ${client.commands.size} commands`, { type: "Watching" });
-        setTimeout(game1, 20000);//these times are in ms, so 30,000 = 30 seconds
-    }      //seconds/1000 = ms
+
 client.on('message', message => {
  if (message.author.bot) return;
  if (!message.content.startsWith(prefix)) return;
